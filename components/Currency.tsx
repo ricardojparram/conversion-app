2c52a5c716644e8bf5539c5d5ea95799fee4696c
Ricardo Parra <ricardo.prxr16@gmail.com>
2025-01-27 23:34:33 -0400

    feat: Currency input now with lable

--- components/Currency.tsx
+++ components/Currency.tsx
@@ -4,7 +4,11 @@ import CurrencyInput, {
 } from "react-native-currency-input";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { Typography } from "@/components/Typography";
+import { Div } from "@/components/Div";
 
+interface CurrencyInputType extends CurrencyInputProps {
+  label?: string;
+}
 export function Currency({
   value,
   onChangeValue,
@@ -12,34 +16,45 @@ export function Currency({
   delimiter = ".",
   separator = ",",
   precision = 2,
+  label,
   onChangeText,
-}: CurrencyInputProps) {
+  onPress,
+}: CurrencyInputType) {
   const textColor = useThemeColor({}, "text");
   // const bgColor = useThemeColor({}, "backgroundSecondary");
 
   return (
-    <CurrencyInput
-      value={value}
-      onChangeValue={onChangeValue}
-      suffix={"  " + suffix}
-      delimiter={delimiter}
-      separator={separator}
-      precision={precision}
-      minValue={0}
-      maxValue={9999999999999}
-      onChangeText={onChangeText}
-      style={{
-        color: textColor,
-        fontSize: 20,
-        textAlign: "right",
-        borderStyle: "solid",
-        borderWidth: 2,
-        borderColor: textColor,
-        borderRadius: 10,
-        paddingRight: 10,
-        height: 50,
-      }}
-    />
+    <Div>
+      {label && (
+        <Typography style={{ paddingLeft: 5 }} type="defaultSemiBold">
+          {label}
+        </Typography>
+      )}
+
+      <CurrencyInput
+        value={value}
+        onChangeValue={onChangeValue}
+        suffix={"  " + suffix}
+        delimiter={delimiter}
+        separator={separator}
+        precision={precision}
+        minValue={0}
+        maxValue={9999999999999}
+        onChangeText={onChangeText}
+        style={{
+          color: textColor,
+          fontSize: 20,
+          textAlign: "right",
+          borderStyle: "solid",
+          borderWidth: 2,
+          borderColor: textColor,
+          borderRadius: 10,
+          paddingRight: 10,
+          height: 50,
+        }}
+        onPress={onPress}
+      />
+    </Div>
   );
 }
 
