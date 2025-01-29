eabf338d9baf5f0551d5e15cf261586d48ab4b33
Ricardo Parra <ricardo.prxr16@gmail.com>
2025-01-29 19:21:01 -0400

    feat: scrollDiv component added

---
+++ components/ScrollDiv.tsx
@@ -0,0 +1,30 @@
+import { ScrollView, type ScrollViewProps, StatusBar } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+export type ThemedScrollViewProps = ScrollViewProps & {
+  lightColor?: string;
+  darkColor?: string;
+};
+export function ScrollDiv({
+  style,
+  lightColor,
+  darkColor,
+  ...otherProps
+}: ThemedScrollViewProps) {
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "background",
+  );
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{ flex: 1, paddingTop: StatusBar.currentHeight }}
+        edges={["top"]}
+      >
+        <ScrollView style={[{ backgroundColor }, style]} {...otherProps} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
