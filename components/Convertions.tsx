ab86cf96b9532399eaaad8da0ef3adf39e00ec44
Ricardo Parra <ricardo.prxr16@gmail.com>
2025-01-29 17:58:02 -0400

    feat: Convertions redesign

--- components/Convertions.tsx
+++ components/Convertions.tsx
@@ -1,6 +1,8 @@
 import { Typography } from "@/components/Typography";
 import { Div } from "@/components/Div";
-import { Currency, TextCurrency } from "@/components/Currency";
+import { TextCurrency } from "@/components/Currency";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { Btn } from "@/components/Btn";
 
 export const ConversionDisplay = ({
   label,
@@ -12,11 +14,50 @@ export const ConversionDisplay = ({
   date?: string;
   calculatedUSD: number;
   calculatedBs: number;
-}) => (
-  <Div>
-    <Typography type="subtitle">{label}</Typography>
-    {date && <Typography>{date}</Typography>}
-    <TextCurrency suffix="$  " value={calculatedUSD} />
-    <TextCurrency value={calculatedBs} />
-  </Div>
-);
+}) => {
+  const textColor = useThemeColor({}, "text");
+  const tintColor = useThemeColor({}, "tint");
+  return (
+    <Div
+      style={{
+        borderStyle: "solid",
+        padding: 10,
+        gap: 5,
+      }}
+    >
+      <Div
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignContent: "flex-end",
+        }}
+      >
+        <Typography type="subtitle">{label}</Typography>
+        {date && <Typography style={{ opacity: 0.7 }}>{date}</Typography>}
+      </Div>
+
+      <Div>
+        <Typography>
+          <TextCurrency suffix="$ " value={calculatedUSD} /> ={"  "}
+          <TextCurrency value={calculatedBs} />
+        </Typography>
+      </Div>
+
+      <Div style={{ flexDirection: "row", gap: 20 }}>
+        <Btn
+          icon="clipboard-multiple-outline"
+          onPress={() => console.log("copiar")}
+        >
+          USD
+        </Btn>
+
+        <Btn
+          icon="clipboard-multiple-outline"
+          onPress={() => console.log("copiar")}
+        >
+          BS
+        </Btn>
+      </Div>
+    </Div>
+  );
+};
