3eed06d1c3f9fa69a553ac1567e0c14b4d762316
Ricardo Parra <ricardo.prxr16@gmail.com>
2025-01-29 16:42:21 -0400

    feat: formatDate refac

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -7,16 +7,15 @@ export function formatDate(timestamp) {
 
   const dia = String(fecha.getDate()).padStart(2, "0"); // Asegura 2 dígitos
   const mes = String(fecha.getMonth() + 1).padStart(2, "0"); // Los meses van de 0 a 11
-  const año = fecha.getFullYear();
+  const año = fecha.getFullYear().toString().slice(-2);
 
   let horas = fecha.getHours();
   const minutos = String(fecha.getMinutes()).padStart(2, "0");
-  const segundos = String(fecha.getSeconds()).padStart(2, "0");
   const ampm = horas >= 12 ? "PM" : "AM";
 
   horas = horas % 12;
   horas = horas ? horas : 12;
   horas = String(horas).padStart(2, "0");
 
-  return `${dia}/${mes}/${año} ${horas}:${minutos}:${segundos} ${ampm}`;
+  return `${dia}/${mes}/${año} ${horas}:${minutos} ${ampm}`;
 }
