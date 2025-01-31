72c772fa231f3b41c60e1a94308885665927746b
Ricardo Parra <ricardo.prxr16@gmail.com>
2025-01-30 20:42:55 -0400

    feat: Index with some changes

--- app/index.tsx
+++ app/index.tsx
@@ -2,22 +2,26 @@ import { useState, useEffect } from "react";
 import { Typography } from "@/components/Typography";
 import { Div } from "@/components/Div";
 import { ScrollDiv } from "@/components/ScrollDiv";
-import { Currency, TextCurrency } from "@/components/Currency";
+import { Currency } from "@/components/Currency";
 import { ConversionDisplay } from "@/components/Convertions";
 import { calculateConversions } from "@/utils/calculateConvertions";
 import { convertionStore } from "@/store/convertions";
 import { CalculatedConvertions } from "@/types/convertions";
-import { IconBtn } from "@/components/Btn";
+import { IconBtn, Btn } from "@/components/Btn";
 import { ActivityIndicator, Dimensions } from "react-native";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 const { height: ScreenHeight, width: ScreenWidth } = Dimensions.get("window");
 export default function Index() {
+  const iconColor = useThemeColor({}, "icon");
   const [bs, setBs] = useState<number | null>(0);
   const [usd, setUSD] = useState<number | null>(0);
   const [isFetching, setIsFetching] = useState(false);
   const convertions = convertionStore((state) => state.convertions);
   const fetchConvertions = convertionStore((state) => state.fetchConvertions);
   const fetch = async () => {
+    setBs(0);
+    setUSD(0);
     setIsFetching(true);
     await fetchConvertions();
     setIsFetching(false);
@@ -45,117 +49,117 @@ export default function Index() {
   }, [usd, bs, convertions]);
 
   return (
-    <>
-      <ScrollDiv>
+    <ScrollDiv>
+      <Div
+        style={[
+          {
+            width: "100%",
+            height: ScreenHeight,
+          },
+          ScreenWidth > 500 && {
+            justifyContent: "center",
+            alignItems: "center",
+          },
+        ]}
+      >
         <Div
-          style={[
-            {
-              width: "100%",
-              height: ScreenHeight,
-            },
-            ScreenWidth > 500 && {
-              justifyContent: "center",
-              alignItems: "center",
-            },
-          ]}
+          style={{
+            alignItems: "center",
+            paddingTop: 30,
+            maxWidth: 400,
+          }}
         >
           <Div
             style={{
-              alignItems: "center",
-              paddingTop: 30,
-              maxWidth: 400,
+              gap: 10,
+              width: "100%",
+              paddingVertical: 10,
+              paddingHorizontal: 40,
             }}
           >
             <Div
               style={{
-                gap: 10,
                 width: "100%",
-                paddingVertical: 10,
-                paddingHorizontal: 40,
+                justifyContent: "space-between",
+                flexDirection: "row",
+                height: 50,
               }}
             >
-              <Div
+              <Typography
+                type="title"
                 style={{
-                  width: "100%",
-                  justifyContent: "space-between",
-                  flexDirection: "row",
+                  textAlignVertical: "center",
+                  display: "flex",
+                  alignItems: "center",
                 }}
               >
-                <Typography
-                  type="title"
-                  style={{ textAlignVertical: "center" }}
-                >
-                  Cambio rápido
-                </Typography>
+                Cambio rápido
+              </Typography>
 
-                <IconBtn onPress={fetch} icon="refresh" size={30} />
+              <Div>
+                {isFetching ? (
+                  <ActivityIndicator
+                    size="large"
+                    color={iconColor}
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  />
+                ) : (
+                  <IconBtn onPress={fetch} icon="refresh" size={35} />
+                )}
               </Div>
-              <Currency
-                value={bs}
-                onChangeValue={setBs}
-                onPress={() => setUSD(0)}
-                onClick={() => setUSD(0)}
-                label="Bolívares."
-              />
-
-              <Currency
-                suffix="$  "
-                value={usd}
-                onChangeValue={setUSD}
-                onPress={() => setBs(0)}
-                onClick={() => setBs(0)}
-                label="Dólares."
-              />
             </Div>
+            <Currency
+              value={bs}
+              onChangeValue={setBs}
+              onPress={() => setUSD(0)}
+              onClick={() => setUSD(0)}
+              label="Bolívares."
+            />
 
-            <Div
-              style={{
-                gap: 10,
-                width: "100%",
-                paddingVertical: 10,
-                paddingHorizontal: 40,
-                justifyContent: "center",
-              }}
-            >
-              <ConversionDisplay
-                label="Paralelo"
-                date={convertions.dateParalelo}
-                calculatedUSD={calculatedConvertions.calculatedUSD.paralelo}
-                calculatedBs={calculatedConvertions.calculatedBs.paralelo}
-              />
+            <Currency
+              suffix="$  "
+              value={usd}
+              onChangeValue={setUSD}
+              onPress={() => setBs(0)}
+              onClick={() => setBs(0)}
+              label="Dólares."
+            />
+          </Div>
 
-              <ConversionDisplay
-                label="BCV"
-                date={convertions.dateBcv}
-                calculatedUSD={calculatedConvertions.calculatedUSD.bcv}
-                calculatedBs={calculatedConvertions.calculatedBs.bcv}
-              />
-              <ConversionDisplay
-                label="Promedio"
-                calculatedUSD={calculatedConvertions.calculatedUSD.promedio}
-                calculatedBs={calculatedConvertions.calculatedBs.promedio}
-              />
-            </Div>
+          <Div
+            style={{
+              gap: 10,
+              width: "100%",
+              paddingVertical: 10,
+              paddingHorizontal: 40,
+              justifyContent: "center",
+            }}
+          >
+            <ConversionDisplay
+              label="Paralelo"
+              date={convertions.dateParalelo}
+              calculatedUSD={calculatedConvertions.calculatedUSD.paralelo}
+              calculatedBs={calculatedConvertions.calculatedBs.paralelo}
+            />
+
+            <ConversionDisplay
+              label="BCV"
+              date={convertions.dateBcv}
+              calculatedUSD={calculatedConvertions.calculatedUSD.bcv}
+              calculatedBs={calculatedConvertions.calculatedBs.bcv}
+            />
+            <ConversionDisplay
+              label="Promedio"
+              calculatedUSD={calculatedConvertions.calculatedUSD.promedio}
+              calculatedBs={calculatedConvertions.calculatedBs.promedio}
+            />
           </Div>
         </Div>
-      </ScrollDiv>
-
-      {isFetching && (
-        <Div
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-          }}
-        >
-          <ActivityIndicator size="large" color="white" />
-        </Div>
-      )}
-    </>
+      </Div>
+    </ScrollDiv>
   );
 }
